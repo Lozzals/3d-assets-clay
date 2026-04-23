598fa0b7a776aa48f13bde8272290915a9a9faa1
Lozzals <lauren@claymates.org>
2026-04-23 15:50:50 +0100

    Update meta tags and config for Clay Nation

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/3d-assets-clay">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
